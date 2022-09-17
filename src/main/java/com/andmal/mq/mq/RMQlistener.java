8675050b341472ebfe9b7b0a4ccaa0e2eed65f19
malandr <quadr988@gmail.com>
2022-09-17 18:50:26 +0300

    init

--- src/main/java/com/andmal/mq/mq/RMQlistener.java
+++ src/main/java/com/andmal/mq/mq/RMQlistener.java
@@ -1,14 +1,21 @@
-//package com.example.sbmessaging.mq;
-//
-//import org.springframework.amqp.rabbit.annotation.RabbitHandler;
-//import org.springframework.amqp.rabbit.annotation.RabbitListener;
-//import org.springframework.stereotype.Service;
-//
-//@RabbitListener(queues = "queue1")
-//public class RMQlistener {
-//
-//    @RabbitHandler
-//    public void processMsg(String msg) {
-//        System.out.println(">>> listening to msg :: " + msg);
-//    }
-//}
+package com.andmal.mq.mq;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RMQlistener {
+
+    @RabbitHandler
+    @RabbitListener(queues = "queue.one")
+    public void processMsg(String msg) {
+        System.out.println(">>> listening to msg :: " + msg);
+    }
+    @RabbitHandler
+    @RabbitListener(queues = "hello")
+    public void listenToHello(String msg) {
+        System.out.println(">>> listening to HELLO msg :: " + msg);
+    }
+
+}
--- src/main/java/com/andmal/mq/mq/ReceiverOfficial.java
+++ src/main/java/com/andmal/mq/mq/ReceiverOfficial.java
@@ -1,13 +1,13 @@
-package com.andmal.mq.mq;
-
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-
-@RabbitListener(queues = "hello")
-public class ReceiverOfficial {
-
-    @RabbitHandler
-    public void receive(String msg) {
-        System.out.println(" [x] Received '" + msg + "'");
-    }
-}
+//package com.andmal.mq.mq;
+//
+//import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+//import org.springframework.amqp.rabbit.annotation.RabbitListener;
+//
+//@RabbitListener(queues = "queue.one")
+//public class ReceiverOfficial {
+//
+//    @RabbitHandler
+//    public void receive(String msg) {
+//        System.out.println(" [x] Received '" + msg + "'");
+//    }
+//}
