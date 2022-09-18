6b5a724bc70ee636565bc49fc9be1224521a2591
malandr <quadr988@gmail.com>
2022-09-18 16:36:05 +0300

    init

--- src/main/java/com/andmal/mq/mq/RMQlistener.java
+++ src/main/java/com/andmal/mq/mq/RMQlistener.java
@@ -1,21 +1,21 @@
-//package com.andmal.mq.mq;
-//
-//import org.springframework.amqp.rabbit.annotation.RabbitHandler;
-//import org.springframework.amqp.rabbit.annotation.RabbitListener;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class RMQlistener {
-//
-//    @RabbitHandler
-//    @RabbitListener(queues = "queue.one")
-//    public void processMsg(String msg) {
-//        System.out.println(">>> listening to msg :: " + msg);
-//    }
-//    @RabbitHandler
-//    @RabbitListener(queues = "hello")
-//    public void listenToHello(String msg) {
-//        System.out.println(">>> listening to HELLO msg :: " + msg);
-//    }
-//
-//}
+package com.andmal.mq.mq;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RMQlistener {
+
+    @RabbitHandler
+    @RabbitListener(queues = "queue.one")
+    public void processMsg(String msg) {
+        System.out.println(">>> listening to msg :: " + msg);
+    }
+    @RabbitHandler
+    @RabbitListener(queues = "hello")
+    public void listenToHello(String msg) {
+        System.out.println(">>> listening to HELLO msg :: " + msg);
+    }
+
+}
